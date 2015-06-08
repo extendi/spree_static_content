000f7e0bf56e58891363e493c306fd262b3287eb
Matteo Alessani <alessani@gmail.com>
2015-06-08 16:34:49 +0200

    added path

--- app/helpers/spree/pages_helper.rb
+++ app/helpers/spree/pages_helper.rb
@@ -3,4 +3,27 @@ module Spree::PagesHelper
     page = Spree::Page.find_by_slug(slug)
     raw page.body if page
   end
+  
+  def static_page_path slug
+    page = Spree::Page.where(slug: slug).first
+    if page.present?
+      static_path page.slug 
+    end
+  end
+  
+  def careers_path
+    
+  end
+  
+  def privacy_path
+    
+  end
+  
+  def faq_path
+    static_page_path 'faq'
+  end
+  
+  def know_how_path
+    
+  end
 end
